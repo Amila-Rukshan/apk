86dd72958db2b85ca6df7e2d2ac897a6330edd37
Amila Senadheera <amila.15@cse.mrt.ac.lk>
2023-03-03 18:28:33 +0530

    test change detection

---
+++ test/integration/scripts/build-components.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
+#
+# WSO2 LLC. licenses this file to you under the Apache License,
+# Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+if git diff --quiet HEAD~1..HEAD -- 'adapter'; then
+    echo "No changes detected in 'adapter'"
+else
+    echo "Changes detected in 'adapter'"
+    # Run your command here
+fi
+
+if git diff --quiet HEAD~1..HEAD -- 'gateway'; then
+    echo "No changes detected in 'gateway'"
+else
+    echo "Changes detected in 'gateway'"
+    # Run your command here
+fi
